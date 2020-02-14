b98a45b751523deeb14c16f29a8e109a8ce85ad0
Andrey Bochkov <a.bochkov@t-systems.com>
2020-02-14 21:13:38 +0300

    optimize - eliminate all double calculations

--- src/lc/recursion/PascalTriangle.java
+++ src/lc/recursion/PascalTriangle.java
@@ -16,25 +16,32 @@ public class PascalTriangle {
         if (j > i) {
             throw new IndexOutOfBoundsException("column > row");
         }
-        return helper(i, j, 0)[0];
+        return helper(i, j, new int[i+1])[0];
     }
 
-    private static int[] helper(int i, int j, int p) {
+    private static int[] helper(int i, int j, int[] p) {
         if (j < 1 || j > i) {
-            return new int[] {0, 0};
+            p[0] = 0;
+            return p;
         }
         if (j == 1) {
-            return new int[]{1, 0};
+            p[0] = 1;
+            p[i] = 0;
+            return p;
         } else if (j == i) {
-            return new int[]{1, 1};
+            p[0] = 1;
+            p[i] = 1;
+            return p;
         }
-        int[] r;
-        if (p > 0) {
-            r = new int[] {p, 0};
+        final int r;
+        if (p[i] > 0) {
+            r = p[i]; // restore left result from previous calls
         } else {
-            r = helper(i - 1, j, p);
+            r = helper(i - 1, j, p)[0]; // go right
         }
-        final int[] l = helper(i - 1, j - 1, r[1]);
-        return new int[] {r[0] + l[0], l[0]};
+        final int l = helper(i - 1, j - 1, p)[0]; //  go left
+        p[0] = r + l; // total for current node
+        p[i] = l; // store left result
+        return p;
     }
 }
